1eac907ac894cb1cf9e6c9fe5942b8abe665d588
Hash <unknownk0420@gmail.com>
2026-02-10 10:42:35 +0900

    BOJ_4949_균형잡힌 세상.java

---
+++ Algorithm/Stack/BOJ_4949_균형잡힌 세상.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+
+            String s = br.readLine();
+            boolean isValid = true;
+
+            Stack<Character> stack = new Stack<>();
+
+            if (s.equals(".")) {
+                break;
+            }
+
+            for (int i = 0; i < s.length(); i++) {
+
+                char c = s.charAt(i);
+
+                if (c == '(' || c == '[') {
+                    stack.push(c);
+                } else if (c == ')'){
+                    if (stack.isEmpty() || stack.peek() != '(') {
+                        isValid = false;
+                        break;
+                    }
+                    stack.pop();
+                } else if (c == ']') {
+                    if (stack.isEmpty() || stack.peek() != '[') {
+                        isValid = false;
+                        break;
+                    }
+                    stack.pop();
+                }
+            }
+            System.out.println(stack.isEmpty() && isValid ? "yes" : "no");
+        }
+    }
+}
